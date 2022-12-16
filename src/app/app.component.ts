13cc80a02665b09c29ca9bdb8259645939ea1106
Linh <huyenlinhvu.pro@gmail.com>
2022-12-16 16:23:39 +0100

    api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'api';
+export class AppComponent implements OnInit {
+  title = 'angularhttp';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.onGetUsers();
+    this.onGetUser();
+  }
+
+  onGetUsers(): void {
+    this.userService.getUsers().subscribe(
+      (response) => console.log(response),
+      (error: any) => console.log(error),
+      () => console.log('Done getting users'),
+    );
+  }
+
+  onGetUser(): void {
+    this.userService.getUser().subscribe(
+      (response) => console.log(response),
+      (error: any) => console.log(error),
+      () => console.log('Done getting user'),
+    );
+  }
 }
---
+++ src/app/service/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../interface/user';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { } 
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
+  }
+
+  getUser(): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/users/1`);
+  }
+}
